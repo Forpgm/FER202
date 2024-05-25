8bebfeac36c7a6ccccec6a09053da5dfa8cecb46
phm.giamy@gmail.com <phm.giamy@gmail.com>
2024-05-25 08:10:01 +0700

    hook context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import "./App.css";
 // import Content from "./components/Content/Content";
-import Navbar from "./components/Navbar/Navbar";
+//import Navbar from "./components/Navbar/Navbar";
 // import Footer from "./components/Footer/Footer";
 // import MainComponent from "./components/shared/MainComponent";
 // import CountDemoClass from "./components/CountDemoClass";
 // import CountDemoFunc from "./components/CountDemoFunc";
-import PlayersWithMap from "./components/PlayersWithMap";
+// import PlayersWithMap from "./components/PlayersWithMap";
+import { useState, useContext, createContext } from "react";
+const moneyFromGrand = createContext();
 function App() {
+  const [money, setMoney] = useState("2 toi");
   return (
-    <div className="container">
-      <Navbar />
-      {/* <Content /> */}
-      {/* <MainComponent />
-      <Footer /> */}
-      {/* <CountDemoClass />
-      <CountDemoFunc /> */}
-      <PlayersWithMap />
-    </div>
+    <>
+      <moneyFromGrand.Provider value={money}>
+        <Children />
+      </moneyFromGrand.Provider>
+    </>
   );
 }
 
+function Grand(money) {
+  return (
+    <>
+      <Parent />
+    </>
+  );
+}
+
+function Parent() {
+  return (
+    <>
+      <Children />
+    </>
+  );
+}
+
+function Children() {
+  const value = useContext(moneyFromGrand);
+  return <>{value}</>;
+}
 export default App;
